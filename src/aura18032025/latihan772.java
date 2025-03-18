f73de1ea8939c797503aad4da93305d3c48ae4ff
Aura Zhulia <aurazhulia@gmail.com>
2025-03-18 10:04:44 +0700

    Add files via upload

---
+++ src/aura18032025/latihan772.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LAB-MM
+ */
+import javax.swing.JOptionPane;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class latihan772 {
+  public static void main(String[] args) throws IOException {
+        // Membuat objek BufferedReader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // Membuat array untuk menyimpan 10 nomor
+        int[] nomor = new int[10];
+
+        // Meminta 10 nomor dari user
+        for (int i = 0; i < 10; i++) {
+            String input = JOptionPane.showInputDialog("Masukkan nomor ke-" + (i + 1));
+            nomor[i] = Integer.parseInt(input);
+        }
+
+        // Mencari nomor terbesar
+        int max = nomor[0];
+        for (int i = 1; i < 10; i++) {
+            if (nomor[i] > max) {
+                max = nomor[i];
+            }
+        }
+
+        // Menampilkan nomor terbesar
+        JOptionPane.showMessageDialog(null, "Nomor terbesar adalah: " + max);
+    }  
+}
